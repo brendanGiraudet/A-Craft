39b4f40000cb996752d8d073a15f197948f27854
brendanGiraudet <brendan.giraudet1@gmail.com>
2018-10-20 08:31:21 +0200

    first commit

--- main.cs
+++ main.cs
@@ -9,67 +9,26 @@ using System.Collections.Generic;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-class Solution
+class Player
 {
     static void Main(string[] args)
     {
-        // opponent
-        string opBowls = Console.ReadLine();
-        var opList = opBowls.Split(' ').ToList().Select(val => Convert.ToInt32(val)).ToList();
-        // me
-        string myBowls = Console.ReadLine();
-        var myList = myBowls.Split(' ').ToList().Select(val => Convert.ToInt32(val)).ToList();
-        // index of choosen bowl
-        int num = int.Parse(Console.ReadLine());
-        // get grains
-        int nbGrains = myList[num];
-        myList[num] = 0;
-        num++;
-        var needReplay = false;
-        do
+        for (int i = 0; i < 10; i++)
         {
-            // my foreach
-            for (int i = num; i < 7 && nbGrains != 0; i++)
-            {
-                nbGrains--;
-                myList[i]++;
-                needReplay = (i + 1 == 7 && nbGrains == 0);
-            }
-            num = 0;
-            // opponent foreach
-            for (int j = 0; j < 6 && nbGrains != 0; j++)
-            {
-                nbGrains--;
-                opList[j]++;
-            }
-        } while (nbGrains != 0);
-        // display opponent bowls
-        DisplayBowls(opList);
-        // display my bowls
-        DisplayBowls(myList);
-        // replay
-        if(needReplay)
-        {
-            System.Console.WriteLine("REPLAY");
+            string line = Console.ReadLine();
         }
-    }
+        int robotCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < robotCount; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int x = int.Parse(inputs[0]);
+            int y = int.Parse(inputs[1]);
+            string direction = inputs[2];
+        }
 
-    static void DisplayBowls(List<int> bowlList)
-    {
-        var line = "";
-        int index = 0;
-        bowlList.ForEach( b => {
-            // if last bowl => reserve
-            if(index == bowlList.Count -1)
-            {
-                line += " [" + b + "]";
-            }
-            else
-            {
-                line += " " + b;
-            }
-            index++;
-        });
-        System.Console.WriteLine(line.Trim());
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("0 0 U 1 1 R 2 2 D 3 3 L");
     }
 }
